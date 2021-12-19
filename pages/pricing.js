3412a647b2c1cf9fe3da8a8a3ca0d87f113be978
Igor Kirei <igorkirei@Igors-MacBook-Pro.local>
2021-12-19 21:32:52 +0200

    updated Pricing component

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,5 +1,22 @@
+import { useContext } from "react";
+
+import Spinner from "../components/_common/Spinner";
+import Pricing from "../components/Pricing";
+
+import { AuthContext } from "../context/auth";
+
 const Component = () => {
-  return <div>Pricing</div>;
+  const { authPending } = useContext(AuthContext);
+
+  if (authPending) {
+    return (
+      <div className="page">
+        <Spinner />
+      </div>
+    );
+  }
+
+  return <Pricing />;
 };
 
 export default Component;
