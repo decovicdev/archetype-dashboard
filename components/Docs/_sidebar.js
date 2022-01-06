3fdec8c6b348bb564c6b0cc14074e55e754eecab
IgorKirei <igor.kirey@toptal.com>
2022-01-06 14:43:26 +0200

    added minor fixes

--- components/Docs/_sidebar.js
+++ components/Docs/_sidebar.js
@@ -1,22 +1,34 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
+import classnames from "classnames";
 
 const Sidebar = () => {
+  const router = useRouter();
+
+  const links = [
+    { name: "Getting Started", link: "/docs/getting-started" },
+    { name: "Authentication", link: "/docs/authentication" },
+    { name: "Installation", link: "/docs/installation" },
+    { name: "Configuring SDK", link: "/docs/configuring-sdk" },
+  ];
+
   return (
     <div className={"sidebar-block"}>
       <div className={"title"}>Getting Started</div>
       <nav>
-        <Link href={"/docs/getting-started"}>
-          <a className="btn small gradient-blue">Quickstart</a>
-        </Link>
-        <Link href={"/docs/authentication"}>
-          <a className="btn small gradient-blue">Authentication</a>
-        </Link>
-        <Link href={"/docs/installation"}>
-          <a className="btn small gradient-blue">Installation</a>
-        </Link>
-        <Link href={"/docs/configuring-sdk"}>
-          <a className="btn small gradient-blue">Configuring SDK</a>
-        </Link>
+        {links.map((link, i) => {
+          return (
+            <Link key={i} href={link.link}>
+              <a
+                className={classnames({
+                  active: router.pathname === link.link,
+                })}
+              >
+                {link.name}
+              </a>
+            </Link>
+          );
+        })}
       </nav>
     </div>
   );
