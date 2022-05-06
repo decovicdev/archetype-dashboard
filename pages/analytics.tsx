3ad85ce86180bd813a1d37a7734180e6067de81d
Mohamed Elbaz <mohamed.elbaz@toptal.com>
2022-05-06 07:17:28 +0200

    replace recharts with chartJs in analytics page

--- pages/analytics.tsx
+++ pages/analytics.tsx
@@ -1,35 +1,34 @@
 import Head from 'next/head';
 
 import { Table } from 'antd';
+// import { useQuery } from 'react-query';
 import {
-  BarChart,
-  LineChart,
-  Line,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-  AreaChart,
-  Area,
-  PieChart,
-  Pie,
-  Cell
-} from 'recharts';
-import { useQuery } from 'react-query';
+  Bar as BarChartJs,
+  Line as LineChartJs,
+  Doughnut
+} from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  PointElement,
+  LineElement,
+  ArcElement,
+  Tooltip as TooltipChartJs,
+  Legend as LegendChartJs
+} from 'chart.js';
 import {
   endpointLatencyData,
   mprByPlan,
-  mrpOverviewData,
   requestsBreakdownData,
   columns,
   endpointOverview
 } from '../helpers/data';
 import config from 'config';
 import DashboardLayout from 'components/_layout/DashboardLayout';
-import AnalyticsService from 'services/analytics.service';
+// import AnalyticsService from 'services/analytics.service';
 
 const COLORS = [
   '#1f6d92',
@@ -40,234 +39,123 @@ const COLORS = [
   '#b0a368'
 ];
 
-const Component = () => {
-  const { data, isLoading, error } = useQuery(
-    'appStats',
-    AnalyticsService.getAppMrr
-  );
-  console.log({ data, isLoading, error });
-  return (
-    <DashboardLayout>
-      <Head>
-        <title>Analytics - {config.meta.title}</title>
-      </Head>
-      <div
-        style={{
-          background: 'white',
-          borderRadius: '10px',
-          padding: '30px 0 50px 0',
-          margin: '30px auto',
-          width: '100%'
-        }}
-      >
-        <h1
-          style={{
-            padding: '0 40px 20px',
-            borderBottom: 'solid 1px #eee',
-            marginBottom: '40px'
-          }}
-        >
-          Requests Breakdown
-        </h1>
-        <ResponsiveContainer width="100%" height={400}>
-          <BarChart
-            width={500}
-            height={300}
-            data={requestsBreakdownData}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="time" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="requests" fill="#baa365" />
-          </BarChart>
-        </ResponsiveContainer>
-      </div>
-      <div
-        style={{
-          background: 'white',
-          borderRadius: '10px',
-          padding: '30px 0 50px 0',
-          margin: '30px auto',
-          width: '100%'
-        }}
-      >
-        <h1
-          style={{
-            padding: '0 40px 20px',
-            borderBottom: 'solid 1px #eee',
-            marginBottom: '40px'
-          }}
-        >
-          Endpoints Latency
-        </h1>
-        <ResponsiveContainer width="100%" height={400}>
-          <LineChart width={500} height={300}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-              dataKey="time"
-              type="category"
-              allowDuplicatedCategory={false}
-            />
-            <YAxis dataKey="requests" />
-            <Tooltip />
-            <Legend />
-            {endpointLatencyData.map((s, index) => (
-              <Line
-                dataKey="requests"
-                data={s.data}
-                name={s.name}
-                key={s.name}
-                fill={COLORS[index]}
-                stroke={COLORS[index]}
-              />
-            ))}
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
-      <div
-        style={{
-          background: 'white',
-          borderRadius: '10px',
-          padding: '30px 0 50px 0',
-          margin: '30px auto',
-          width: '100%'
-        }}
-      >
-        <h1
-          style={{
-            padding: '0 40px 20px',
-            borderBottom: 'solid 1px #eee',
-            marginBottom: '40px'
-          }}
-        >
-          MRP Overview
-        </h1>
-        <ResponsiveContainer width="100%" height={400}>
-          <AreaChart
-            width={500}
-            height={400}
-            data={mrpOverviewData}
-            margin={{
-              top: 10,
-              right: 30,
-              left: 0,
-              bottom: 0
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Area
-              type="monotone"
-              dataKey="churn"
-              stackId="1"
-              stroke="#fab7b4"
-              fill="#fab7b4"
-            />
-            <Area
-              type="monotone"
-              dataKey="downgrades"
-              stackId="1"
-              stroke="#ffcbae"
-              fill="#ffcbae"
-            />
-            <Area
-              type="monotone"
-              dataKey="existing"
-              stackId="1"
-              stroke="#16b5d6"
-              fill="#16b5d6"
-            />
-            <Area
-              type="monotone"
-              dataKey="upgrades"
-              stackId="1"
-              stroke="#9de6d4"
-              fill="#9de6d4"
-            />
-            <Area
-              type="monotone"
-              dataKey="new"
-              stackId="1"
-              stroke="#00c3b1"
-              fill="#00c3b1"
-            />
-          </AreaChart>
-        </ResponsiveContainer>
-      </div>
-      <div
-        style={{
-          background: 'white',
-          borderRadius: '10px',
-          padding: '30px 0 50px 0',
-          margin: '30px auto',
-          width: '100%'
-        }}
-      >
-        <h1
-          style={{
-            padding: '0 40px 20px',
-            borderBottom: 'solid 1px #eee',
-            marginBottom: '40px'
-          }}
-        >
-          MRP by Plan
-        </h1>
-        <ResponsiveContainer width="100%" height={400}>
-          <PieChart>
-            <Pie
-              data={mprByPlan}
-              innerRadius={80}
-              outerRadius={100}
-              fill="#8884d8"
-              paddingAngle={5}
-              dataKey="value"
-              label
-            >
-              {mprByPlan.map((entry, index) => (
-                <Cell
-                  key={`cell-${index}`}
-                  fill={COLORS[index % COLORS.length]}
-                />
-              ))}
-            </Pie>
-            <Tooltip />
-            <Legend />
-          </PieChart>
-        </ResponsiveContainer>
-      </div>
-      <div
-        style={{
-          background: 'white',
-          borderRadius: '10px',
-          padding: '30px 0 50px 0',
-          margin: '30px auto',
-          width: '100%'
-        }}
-      >
-        <h1
-          style={{
-            padding: '0 40px 20px',
-            borderBottom: 'solid 1px #eee',
-            marginBottom: '40px'
-          }}
-        >
-          Endpoint Overview
-        </h1>
-        <Table columns={columns} dataSource={endpointOverview} />
-      </div>
-    </DashboardLayout>
-  );
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  PointElement,
+  LineElement,
+  ArcElement,
+  TooltipChartJs,
+  LegendChartJs
+);
+
+const chart1Options = {
+  responsive: true,
+  plugins: {
+    title: {
+      display: true,
+      text: 'Requests Breakdown'
+    },
+    legend: {
+      position: 'bottom' as const
+    }
+  }
 };
+
+const chart1Data = {
+  labels: requestsBreakdownData.map((i) => i.time),
+  datasets: [
+    {
+      label: 'Requests',
+      data: requestsBreakdownData.map((i) => i.requests),
+      backgroundColor: '#baa365'
+    }
+  ]
+};
+
+const chart2Options = {
+  responsive: true,
+  plugins: {
+    title: {
+      display: true,
+      text: 'Endpoints Latency'
+    },
+    legend: {
+      position: 'bottom' as const
+    }
+  }
+};
+
+const chart2Data = {
+  labels: endpointLatencyData[0].data.map((j) => j.time),
+  datasets: endpointLatencyData.map((i, index) => ({
+    label: i.name,
+    data: i.data.map((j) => j.requests),
+    backgroundColor: COLORS[index]
+  }))
+};
+
+const chart3Options = {
+  responsive: true,
+  plugins: {
+    title: {
+      display: true,
+      text: 'MRP by Plan'
+    },
+    legend: {
+      position: 'bottom' as const
+    }
+  }
+};
+
+const chart3Data = {
+  labels: mprByPlan.map((i) => i.name),
+  datasets: [
+    {
+      data: mprByPlan.map((i) => i.value),
+      backgroundColor: mprByPlan.map((i, index) => COLORS[index])
+    }
+  ]
+};
+
+// const { data, isLoading, error } = useQuery(
+//   'appStats',
+//   AnalyticsService.getAppMrr
+// );
+const Component = () => (
+  <DashboardLayout>
+    <Head>
+      <title>Analytics - {config.meta.title}</title>
+    </Head>
+    <BarChartJs options={chart1Options} data={chart1Data} />
+    <LineChartJs options={chart2Options} data={chart2Data} />
+    <div className="flex justify-center px-[300px]">
+      <Doughnut options={chart3Options} data={chart3Data} />
+    </div>
+
+    <div
+      style={{
+        background: 'white',
+        borderRadius: '10px',
+        padding: '30px 0 50px 0',
+        margin: '30px auto',
+        width: '100%'
+      }}
+    >
+      <h1
+        style={{
+          padding: '0 40px 20px',
+          borderBottom: 'solid 1px #eee',
+          marginBottom: '40px'
+        }}
+      >
+        Endpoint Overview
+      </h1>
+      <Table columns={columns} dataSource={endpointOverview} />
+    </div>
+  </DashboardLayout>
+);
 
 export default Component;
