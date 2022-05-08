a5660513a0287095ff7492584202b5c7c9ab9047
arturmasgutov <masagutovartur765@gmail.com>
2022-05-08 07:52:20 -0400

    fix: user edit page, loading, add: formik

--- components/Users/Edit.tsx
+++ components/Users/Edit.tsx
@@ -1,15 +1,12 @@
-import React, { useState, useCallback } from 'react';
-import Head from 'next/head';
+import React from 'react';
 import { useRouter } from 'next/router';
-import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { useQuery } from 'react-query';
 import DeleteUserModal from './DeleteUserModal';
-import config from 'config';
 import Spinner from 'components/_common/Spinner';
 import CustomerService from 'services/customer.service';
 import { useHelpers } from 'context/HelperProvider';
 import useDisclosure from 'hooks/useDisclosure';
 import { ROUTES } from 'constant/routes';
-import BreadCrumbs from 'components/_common/BreadCrumbs';
 import Button from 'components/_common/Button';
 import { ButtonVariant } from 'types/Button';
 import Divider from 'components/_common/Divider';
@@ -17,11 +14,37 @@ import ErrorText from 'components/_typography/ErrorText';
 import Input from 'components/_common/Input';
 import Title from 'components/_typography/Title';
 import { TypographyVariant } from 'types/Typography';
+import { Form, Formik, FormikProvider, useFormik } from 'formik';
+import { object, string } from 'yup';
+
+type FormSchema = {
+  email: string;
+}
 
 const Component = () => {
   const router = useRouter();
   const { showAlert } = useHelpers();
-  const [fields, setFields] = useState(null);
+
+  const formik = useFormik<FormSchema>({
+    initialValues: {
+      email: ''
+    },
+    validationSchema: object().shape({
+      email: string().email().required()
+    }),
+    onSubmit: async (values, helpers) => {
+      try {
+        await CustomerService.updateById(router.query.userId, {
+          email: values.email
+        });
+
+        showAlert('Success!', true);
+      } catch (error) {
+        console.error(error);
+        showAlert('Something went wrong', false);
+      }
+    }
+  })
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
@@ -30,94 +53,46 @@ const Component = () => {
     async () => CustomerService.getById(router.query.userId),
     {
       onError: (e: any) => showAlert(e.message),
-      onSuccess: (newData: any) => {
-        setFields({ email: newData?.email });
+      onSuccess: (user) => {
+        formik.setFieldValue('email', user.email);
       }
     }
   );
 
-  const changeFields = useCallback(
-    (field, value, obj?: any) => {
-      const result = { ...fields };
-
-      if (!field && !value && obj) {
-        for (const key in obj) {
-          result[key] = obj[key];
-        }
-      } else {
-        result[field] = value;
-      }
-
-      setFields(result);
-    },
-    [fields]
-  );
-
-  const queryClient = useQueryClient();
-
-  const { mutate: saveUser, isLoading: isEditUserLoading } = useMutation(
-    async () => {
-      if (isEditUserLoading) return;
-      await CustomerService.updateById(router.query.userId, {
-        email: fields.email
-      });
-    },
-    {
-      onError: (e: any) => showAlert(e.message),
-      onSuccess: async () => {
-        await queryClient.invalidateQueries(['user', router.query.userId]);
-        showAlert('Success', true);
-      }
-    }
-  );
+  if (isLoading) return <Spinner />
 
   return (
     <>
-      <Head>
-        <title>Edit Customer - {config.meta.title}</title>
-      </Head>
-      {isLoading && <Spinner />}
-      <BreadCrumbs
-        links={[
-          { url: ROUTES.USERS.BASE_URL, title: 'Customers' },
-          {
-            url: `${ROUTES.USERS.EDIT}/${router.query.userId}`,
-            title: 'Edit Customer'
-          }
-        ]}
-      />
-      {fields ? (
-        <>
-          <div className="flex items-center">
-            <Title
-              variant={TypographyVariant.dark}
-              level={3}
-              className="!text-left mb-2"
-            >
-              Customer Information
-            </Title>
-            <Button variant={ButtonVariant.danger} onClick={onOpen}>
-              Delete
-            </Button>
-          </div>
+      <div className="flex items-center">
+        <Title
+          variant={TypographyVariant.dark}
+          level={3}
+          className="!text-left mb-2"
+        >
+          Customer Information
+        </Title>
+        <Button variant={ButtonVariant.danger} onClick={onOpen}>
+          Delete
+        </Button>
+      </div>
+
+      <FormikProvider value={formik}>
+        <Form>
           <Input
-            name="email"
             placeholder="Email"
             label="Email"
-            value={fields.email}
-            onChange={(e) => changeFields('email', e.target.value)}
+            {...formik.getFieldProps('email')}
           />
-          <Divider className="my-2" />
+          <ErrorText>{formik.touched.email && formik.errors.email}</ErrorText>
           <div className="w-full flex space-x-2">
-            <Button onClick={() => saveUser()}>Save</Button>
-            <Button variant={ButtonVariant.link} url={ROUTES.USERS.BASE_URL}>
+            <Button type='submit'>Save</Button>
+            <Button variant={ButtonVariant.link} url={`${ROUTES.USERS.BASE_URL}/${router.query.userId}`}>
               Cancel
             </Button>
           </div>
-        </>
-      ) : (
-        <ErrorText>Customer not found.</ErrorText>
-      )}
+        </Form>
+      </FormikProvider>
+      <Divider className="my-2" />
       <DeleteUserModal id="data" isOpen={isOpen} onClose={onClose} />
     </>
   );
--- pages/users/edit/[userId].tsx
+++ pages/users/edit/[userId].tsx
@@ -1,10 +1,32 @@
 import EditUser from 'components/Users/Edit';
+import BreadCrumbs from 'components/_common/BreadCrumbs';
 import DashboardLayout from 'components/_layout/DashboardLayout';
+import config from 'config';
+import { ROUTES } from 'constant/routes';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-const Component = () => (
-  <DashboardLayout>
-    <EditUser />
-  </DashboardLayout>
-);
+const Component = () => {
+  const router = useRouter();
+
+  return (
+    <DashboardLayout>
+      <Head>
+        <title>Edit Customer - {config.meta.title}</title>
+      </Head>
+      <BreadCrumbs
+        links={[
+          { url: ROUTES.USERS.BASE_URL, title: 'Customers' },
+          { url: `${ROUTES.USERS.BASE_URL}/${router.query.userId}`, title: 'Customer' },
+          {
+            url: `${ROUTES.USERS.EDIT}/${router.query.userId}`,
+            title: 'Edit Customer'
+          }
+        ]}
+      />
+      <EditUser />
+    </DashboardLayout>
+  )
+};
 
 export default Component;
